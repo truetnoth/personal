3f772a020071af6efc31e14a1d1aad5a60924a13
Valera Trusevich <truetnoth@gmail.com>
2026-05-13 17:00:29 +0300

    Add dedicated topic pages

--- _plugins/writing_posts.rb
+++ _plugins/writing_posts.rb
@@ -27,19 +27,44 @@ module WritingPosts
       .uniq
   end
 
+  def slug(value)
+    value.to_s
+      .strip
+      .downcase
+      .gsub(/[^a-z0-9а-яё\-\/_]+/i, "-")
+      .gsub(/-+/, "-")
+      .gsub(%r{\A/+|/+\z}, "")
+  end
+
+  def topic_path(tag)
+    "/topics/#{slug(tag)}/"
+  end
+
   def apply_permalink(document)
     raw_url = document.data["url"].to_s.strip
     raw_url = File.basename(document.basename, File.extname(document.basename)) if raw_url.empty?
 
-    slug = raw_url
-      .downcase
-      .gsub(%r{\Ahttps?://[^/]+/?}, "")
-      .gsub(%r{\A/+|/+\z}, "")
-      .gsub(/[^a-z0-9а-яё\-\/_]+/i, "-")
-      .gsub(/-+/, "-")
-      .gsub(%r{/+}, "/")
+    permalink_slug = slug(raw_url.gsub(%r{\Ahttps?://[^/]+/?}, ""))
 
-    document.data["permalink"] = "/#{slug}/"
+    document.data["permalink"] = "/#{permalink_slug}/"
+  end
+end
+
+class WritingTopicPage < Jekyll::Page
+  def initialize(site, base, tag, posts)
+    @site = site
+    @base = base
+    @dir = WritingPosts.topic_path(tag).sub(%r{\A/}, "").sub(%r{/\z}, "")
+    @name = "index.html"
+    @content = ""
+
+    process(@name)
+
+    data["layout"] = "topic"
+    data["title"] = tag
+    data["topic"] = tag
+    data["topic_posts"] = posts
+    data["permalink"] = WritingPosts.topic_path(tag)
   end
 end
 
@@ -58,6 +83,10 @@ module WritingTagFilters
     Array(posts).select do |post|
       Array(post_value(post, "tags")).map(&:to_s).include?(tag.to_s)
     end
+  end
+
+  def writing_tag_url(tag)
+    WritingPosts.topic_path(tag)
   end
 
   private
@@ -82,6 +111,30 @@ class WritingPostsGenerator < Jekyll::Generator
 
     collection.docs.each do |document|
       WritingPosts.apply_defaults(document)
+    end
+  end
+end
+
+class WritingTopicPagesGenerator < Jekyll::Generator
+  priority :normal
+
+  def generate(site)
+    posts = Array(site.collections["writing"]&.docs)
+    tags = posts
+      .flat_map { |post| Array(post.data["tags"]) }
+      .map(&:to_s)
+      .reject(&:empty?)
+      .uniq
+      .sort
+
+    tags.each do |tag|
+      next if WritingPosts.slug(tag).empty?
+
+      tagged_posts = posts.select do |post|
+        Array(post.data["tags"]).map(&:to_s).include?(tag.to_s)
+      end
+      tagged_posts = tagged_posts.sort_by { |post| post.data["date"] || Time.at(0) }.reverse
+      site.pages << WritingTopicPage.new(site, site.source, tag, tagged_posts)
     end
   end
 end
--- pages/topics.md
+++ pages/topics.md
@@ -9,26 +9,9 @@ permalink: /topics/
 {% if tags.size > 0 %}
 <p class="topic-list">
 {% for tag in tags %}
-  <a href="#{{ tag | slugify }}">{{ tag }}</a>{% unless forloop.last %}<span class="muted">,</span>{% endunless %}
+  <a href="{{ tag | writing_tag_url | relative_url }}">{{ tag }}</a>{% unless forloop.last %}<span class="muted">,</span>{% endunless %}
 {% endfor %}
 </p>
-
-{% for tag in tags %}
-### {{ tag }}
-{: #{{ tag | slugify }} }
-
-<ul class="index-list">
-{% assign tagged_posts = posts | where_writing_tag: tag %}
-{% for post in tagged_posts %}
-  <li>
-    <a class="plain" href="{{ post.url | relative_url }}">
-      <span class="index-date muted small font-ui">{{ post.date | ru_date }}</span>
-      <span class="index-title">{{ post.title | escape }}</span>
-    </a>
-  </li>
-{% endfor %}
-</ul>
-{% endfor %}
 {% else %}
 Пока нет тегов.
 {% endif %}
